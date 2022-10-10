fe0d58e313236ef67196fe6cd31de1b6aa5518f5
ItsGeeBee <gracelbrown@hotmail.com>
2022-10-10 15:58:05 +1100

    Debugged checkout issue, added execPopulate() to resolvers.js for checkout

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -13,14 +13,11 @@ const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
 
-
-  const state = useSelector((state) => {
-    return state
-  });
-
   const dispatch = useDispatch();
-
+  const state = useSelector(state => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  
+  
   useEffect(() => {
     if (data) {
       stripePromise.then((res) => {
